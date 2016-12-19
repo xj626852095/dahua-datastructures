a5e471ce43cc0187791de0a78ca195c5d3b8fc9e
xiangjie <xiangjie55@qq.com>
2016-12-20 01:12:03 +0800

    初始化

---
+++ src/com/kevin/datastructures/A线性表.java
@@ -0,0 +1,17 @@
+package com.kevin.datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class A线性表 {
+
+	public static void main(String[] args) {
+		
+		//1. 实现方式为数组， 顺序存储结构
+		List<Integer> nums = new ArrayList<Integer>();
+		Vector<Integer> nums2 = new Vector<Integer>();
+
+	}
+
+}
